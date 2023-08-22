3d396a9abf9ce6466e11e712d7f391694306d5dc
SimoneLagana <lagana.1946083@studenti.uniroma1.it>
2023-08-22 12:03:31 +0200

    Eliminato Schoolstaff

--- app/models/school_staff.rb
+++
@@ -1,4 +0,0 @@
-class SchoolStaff < User
-    validates :birthdate, absence: true
-    validates :student_class_code, absence: true
-end
--- db/migrate/20230822094135_create_school_staffs.rb
+++
@@ -1,8 +0,0 @@
-class CreateSchoolStaffs < ActiveRecord::Migration[7.0]
-  def change
-    create_table :school_staffs, id: false do |t|
-
-      t.timestamps
-    end
-  end
-end

282bd154087b3ee615c457f9504fe7cdcb53633b
jacopoTamarri <tamarri.19469293@studenti.uniroma1.it>
2023-08-22 20:05:24 +0200

    admin controller: add and edit schoolstaff, school

---
+++ app/controllers/oneschool_controller.rb
@@ -0,0 +1,5 @@
+class OneschoolController < ApplicationController
+    def index
+    end
+end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
+  get 'oneschool/index'
+  root "oneschool#index"
+
+  get "/oneschool", to: "oneschool#index"
+
+  #routes for admin:
+  get "/admin/signup", to: "admin#signup"
+  get "admin/manage", to: "admin#manage"
+  post "/admin/create", to: "admin#create" 
+  post "/admin/createSchool", to: "admin#create_school"
+  post "/admin/createStaff", to: "admin#create_staff"
+  post "/admin/editSchool", to: "admin#edit_school"
+  post "/admin/updateSchool", to: "admin#update_school"
+  post "/admin/deleteSchool", to: "admin#delete_school"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # namespace :admin do
+  #   resources :schools, only: [:new, :create, :destroy]
+  # end
+  # namespace :admin do
+  #   resources :users
+  # end
+  
 end
